d7554ee598e880e1135bc352415a887818a0a164
Aaron Rodriguez <aatrodri@ucsc.edu>
2023-11-17 11:30:55 -0800

    Changes

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,6 +3,20 @@ class Play extends Phaser.Scene{
         super("playScene");
     }
 
+    preload(){
+        this.load.image('background', '.assets/DigChampsbackground');
+        this.load.image('shovel', '.assets/this.shovel.png');
+    }
 
-    
+    create(){
+        this.p1Shovel = this.physics.add.sprite(game.config.width /7, game.config.height /2 - borderUISize - borderPadding, 'shovel').setOrigin(0.5, 0);
+
+    }
+        
+
+    update(){
+
+    }
+
+
 }
